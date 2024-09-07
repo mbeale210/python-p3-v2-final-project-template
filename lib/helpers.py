4d0aacabe78ab6a15b88cdd71c32caa68443065f
Mark Beale <bealebeaverst@gmail.com>
2024-09-07 11:44:12 -0700

    helper bare bones

--- lib/helpers.py
+++ lib/helpers.py
@@ -5,5 +5,18 @@ def helper_1():
 
 
 def exit_program():
-    print("Goodbye!")
+    print("May the Force be with You!")
     exit()
+
+def display_all_characters():
+    print()
+
+def add_delete_character():
+    print()
+
+def display_planet_details():
+    print()
+
+def move_character():
+    print()
+
