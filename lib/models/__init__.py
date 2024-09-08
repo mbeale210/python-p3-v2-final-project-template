5d3f0976a9b259b70631dfcbd10466c34854eeb9
Mark Beale <bealebeaverst@gmail.com>
2024-09-07 21:29:31 -0700

    working seed.py

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -1,6 +1,8 @@
+# lib/models/__init__.py
 import sqlite3
-from .characters import Character
+from .character import Character
 from .planet import Planet
+from .base import CONN, CURSOR
 
 CONN = sqlite3.connect('star_wars.db')
 CURSOR = CONN.cursor()
